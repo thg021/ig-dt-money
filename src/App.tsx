a65e59c473f8042f0b36915d8792df21aa363aeb
Thiago Silva <thg021@gmail.com>
2023-01-23 22:29:21 -0300

    feat: estilizacao inicial do tema e font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 function App() {
-  return <div>Hello </div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>Hello </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
